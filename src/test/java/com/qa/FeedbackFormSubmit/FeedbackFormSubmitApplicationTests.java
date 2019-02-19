67320c583eff7ebaae3f52b69bd9f53e216eb581
ahmedqac <ahmed.abidali@hotmail.com>
2019-02-19 12:21:47 +0000

    added new files using initialiser

---
+++ src/test/java/com/qa/FeedbackFormSubmit/FeedbackFormSubmitApplicationTests.java
@@ -0,0 +1,16 @@
+package com.qa.FeedbackFormSubmit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FeedbackFormSubmitApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
