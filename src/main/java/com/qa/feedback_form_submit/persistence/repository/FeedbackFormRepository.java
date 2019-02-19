6257b38887f42659efc9fd699f09d748daa74663
ahmedqac <ahmed.abidali@hotmail.com>
2019-02-19 12:18:49 +0000

    removed old files

--- src/main/java/com/qa/feedback_form_submit/persistence/repository/FeedbackFormRepository.java
+++
@@ -1,9 +0,0 @@
-package com.qa.feedback_form_submit.persistence.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.qa.feedback_form_submit.persistence.domain.FeedbackForm;
-
-public interface FeedbackFormRepository extends JpaRepository<FeedbackForm, Long>{
-
-}
